6655c9126405367489d3f3f7ff494881bc521388
Andrea Giammarchi <andrea.giammarchi@gmail.com>
2021-02-01 18:55:56 +0100

    added href to <link> and <a>

--- esm/html/html-anchor-element.js
+++ esm/html/html-anchor-element.js
@@ -1,10 +1,23 @@
+import {accessorAttribute, registerHTMLClass} from '../utils.js';
 import {HTMLElement} from './html-element.js';
+
+const tagName = 'a';
 
 /**
  * @implements globalThis.HTMLAnchorElement
  */
-export class HTMLAnchorElement extends HTMLElement {
-  constructor(ownerDocument, localName = 'a') {
+class HTMLAnchorElement extends HTMLElement {
+  constructor(ownerDocument, localName = tagName) {
     super(ownerDocument, localName);
   }
+
+  /* c8 ignore start */ // copy paste from img.src, already covered
+  get href() { return accessorAttribute.get(this, 'href'); }
+  set href(value) { accessorAttribute.set(this, 'href', value); }
+  /* c8 ignore stop */
+
 }
+
+registerHTMLClass(tagName, HTMLAnchorElement);
+
+export {HTMLAnchorElement};
--- esm/html/html-link-element.js
+++ esm/html/html-link-element.js
@@ -1,10 +1,23 @@
+import {accessorAttribute, registerHTMLClass} from '../utils.js';
 import {HTMLElement} from './html-element.js';
+
+const tagName = 'img';
 
 /**
  * @implements globalThis.HTMLLinkElement
  */
-export class HTMLLinkElement extends HTMLElement {
-  constructor(ownerDocument, localName = 'link') {
+class HTMLLinkElement extends HTMLElement {
+  constructor(ownerDocument, localName = tagName) {
     super(ownerDocument, localName);
   }
+
+  /* c8 ignore start */ // copy paste from img.src, already covered
+  get href() { return accessorAttribute.get(this, 'href'); }
+  set href(value) { accessorAttribute.set(this, 'href', value); }
+  /* c8 ignore stop */
+
 }
+
+registerHTMLClass(tagName, HTMLLinkElement);
+
+export {HTMLLinkElement};
