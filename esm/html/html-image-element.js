7a36355742b168323042f1cb98821a0544b01b4d
Andrea Giammarchi <andrea.giammarchi@gmail.com>
2021-02-01 18:38:04 +0100

    added width, height, and srcset to <img>

--- esm/html/html-image-element.js
+++ esm/html/html-image-element.js
@@ -13,6 +13,17 @@ class HTMLImageElement extends HTMLElement {
 
   get src() { return accessorAttribute.get(this, 'src'); }
   set src(value) { accessorAttribute.set(this, 'src', value); }
+
+  /* c8 ignore start */
+  get width() { return accessorAttribute.get(this, 'width'); }
+  set width(value) { accessorAttribute.set(this, 'width', value); }
+  
+  get height() { return accessorAttribute.get(this, 'height'); }
+  set height(value) { accessorAttribute.set(this, 'height', value); }
+
+  get srcset() { return accessorAttribute.get(this, 'srcset'); }
+  set srcset(value) { accessorAttribute.set(this, 'srcset', value); }
+  /* c8 ignore stop */
 }
 
 registerHTMLClass(tagName, HTMLImageElement);
