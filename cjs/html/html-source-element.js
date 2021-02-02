f144cf94e0ebecd6a5038ed929a2226aa9e6e958
Malte Ubl <malte.ubl@gmail.com>
2021-02-01 20:11:24 -0800

    Implement additional special case getters/setters
    - `sizes` on HTMLImageElement
    - `src`, `srcset`, `sizes` and `type` on HTMLSourceElement

--- cjs/html/html-source-element.js
+++ cjs/html/html-source-element.js
@@ -8,5 +8,19 @@ class HTMLSourceElement extends HTMLElement {
   constructor(ownerDocument, localName = 'source') {
     super(ownerDocument, localName);
   }
+
+  /* c8 ignore start */
+  get src() { return stringAttribute.get(this, 'src'); }
+  set src(value) { stringAttribute.set(this, 'src', value); }
+
+  get srcset() { return stringAttribute.get(this, 'srcset'); }
+  set srcset(value) { stringAttribute.set(this, 'srcset', value); }
+
+  get sizes() { return stringAttribute.get(this, 'sizes'); }
+  set sizes(value) { stringAttribute.set(this, 'sizes', value); }
+
+  get type() { return stringAttribute.get(this, 'type'); }
+  set type(value) { stringAttribute.set(this, 'type', value); }
+  /* c8 ignore stop */
 }
 exports.HTMLSourceElement = HTMLSourceElement
