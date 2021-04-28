9fb00318089abfb443ae29845c4fca11dab6c846
Minas Keshishyan <minas90@gmail.com>
2021-04-28 20:06:03 +0400

    add test for outerHTML

---
+++ test/interface/element.js
@@ -0,0 +1,18 @@
+const assert = require('../assert.js').for('Text');
+
+const {parseHTML} = global[Symbol.for('linkedom')];
+
+const {document} = parseHTML('<html><div><span></span></div></html>');
+
+let div = document.querySelector('div');
+
+div.firstChild.outerHTML = 'hello';
+assert(div.firstChild.toString(), 'hello');
+
+div.innerHTML = '<span></span>'
+div.firstChild.outerHTML = '<p>hello</p>';
+assert(div.firstChild.toString(), '<p>hello</p>');
+
+div.innerHTML = '<span></span>'
+div.firstChild.outerHTML = '<p>hello</p> world';
+assert(div.toString(), '<div><p>hello</p> world</div>');
