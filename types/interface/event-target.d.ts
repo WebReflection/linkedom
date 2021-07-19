86169fa1ffd9f0217be71498504f5ced33ab2562
Mickael Meausoone <Mickael.Meausoone@hmhco.com>
2021-07-19 11:09:58 +0100

    Event bubbling: _getParent protected
    used "protected" here instead of "private", because Node extend EventTarget and needs to overwrite "_getParent" while still allowing "dispatchEvent" to access it.

--- types/interface/event-target.d.ts
+++ types/interface/event-target.d.ts
@@ -3,6 +3,9 @@ export { DOMEventTarget as EventTarget };
  * @implements globalThis.EventTarget
  */
 declare class DOMEventTarget implements globalThis.EventTarget {
-    _getParent(): any;
+    /**
+     * @protected
+     */
+    protected _getParent(): any;
     dispatchEvent(event: any): any;
 }
