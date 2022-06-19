a6c4042c5c53ee56259ffbcf2a9ebeeb738fe687
Innei <tukon479@gmail.com>
2022-06-19 16:47:08 +0800

    fix: Added script string accessor

--- types/esm/html/script-element.d.ts
+++ types/esm/html/script-element.d.ts
@@ -2,5 +2,19 @@
  * @implements globalThis.HTMLScriptElement
  */
 export class HTMLScriptElement extends TextElement implements globalThis.HTMLScriptElement {
+    set type(arg: any);
+    get type(): any;
+    set src(arg: any);
+    get src(): any;
+    set defer(arg: any);
+    get defer(): any;
+    set crossOrigin(arg: any);
+    get crossOrigin(): any;
+    set nomodule(arg: any);
+    get nomodule(): any;
+    set referrerPolicy(arg: any);
+    get referrerPolicy(): any;
+    set async(arg: any);
+    get async(): any;
 }
 import { TextElement } from "./text-element.js";
