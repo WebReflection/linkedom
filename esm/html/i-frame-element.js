284f34ca0dedd39b4025dd02da7b2704e3af29d1
Ilya Medvedev <iam-medvedev@users.noreply.github.com>
2022-11-15 12:27:36 +0300

    Added `iframe` attributes (#185)
    * Added iframe attributes
    
    * Updated test

--- esm/html/i-frame-element.js
+++ esm/html/i-frame-element.js
@@ -1,5 +1,5 @@
 import {registerHTMLClass} from '../shared/register-html-class.js';
-import {stringAttribute} from '../shared/attributes.js';
+import {booleanAttribute, stringAttribute} from '../shared/attributes.js';
 
 import {HTMLElement} from './element.js';
 
@@ -16,6 +16,24 @@ class HTMLIFrameElement extends HTMLElement {
   /* c8 ignore start */
   get src() { return stringAttribute.get(this, 'src'); }
   set src(value) { stringAttribute.set(this, 'src', value); }
+
+  get srcdoc() { return stringAttribute.get(this, "srcdoc"); }
+  set srcdoc(value) { stringAttribute.set(this, "srcdoc", value); }
+
+  get name() { return stringAttribute.get(this, "name"); }
+  set name(value) { stringAttribute.set(this, "name", value); }
+
+  get allow() { return stringAttribute.get(this, "allow"); }
+  set allow(value) { stringAttribute.set(this, "allow", value); }
+
+  get allowFullscreen() { return booleanAttribute.get(this, "allowfullscreen"); }
+  set allowFullscreen(value) { booleanAttribute.set(this, "allowfullscreen", value); }
+  
+  get referrerPolicy() { return stringAttribute.get(this, "referrerpolicy"); }
+  set referrerPolicy(value) { stringAttribute.set(this, "referrerpolicy", value); }
+  
+  get loading() { return stringAttribute.get(this, "loading"); }
+  set loading(value) { stringAttribute.set(this, "loading", value); }
   /* c8 ignore stop */
 }
 
--- types/esm/html/i-frame-element.d.ts
+++ types/esm/html/i-frame-element.d.ts
@@ -4,5 +4,17 @@
 export class HTMLIFrameElement extends HTMLElement implements globalThis.HTMLIFrameElement {
     set src(arg: any);
     get src(): any;
+    set srcdoc(arg: any);
+    get srcdoc(): any;
+    set name(arg: any);
+    get name(): any;
+    set allow(arg: any);
+    get allow(): any;
+    set allowFullscreen(arg: any);
+    get allowFullscreen(): any;
+    set referrerPolicy(arg: any);
+    get referrerPolicy(): any;
+    set loading(arg: any);
+    get loading(): any;
 }
 import { HTMLElement } from "./element.js";
