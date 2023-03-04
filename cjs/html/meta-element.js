e6059c328a3c0478d025a02fd48f73c5ae50a709
Suraj Yadav <ysuraj877@gmail.com>
2023-03-04 21:46:05 +0530

    Add attributes for `meta` element
    Added the following attributes
    name, httpEquiv, content & charset

--- cjs/html/meta-element.js
+++ cjs/html/meta-element.js
@@ -1,12 +1,31 @@
 'use strict';
 const {HTMLElement} = require('./element.js');
+const {registerHTMLClass} = require('../shared/register-html-class.js');
+const { stringAttribute } = require('../shared/attributes.js');
 
+const tagName = 'meta'
 /**
  * @implements globalThis.HTMLMetaElement
  */
 class HTMLMetaElement extends HTMLElement {
-  constructor(ownerDocument, localName = 'meta') {
+  constructor(ownerDocument, localName =tagName) {
     super(ownerDocument, localName);
   }
+
+  get name() { return stringAttribute.get(this, 'name'); }
+  set name(value) { stringAttribute.set(this, 'name', value); }
+
+  get httpEquiv() { return stringAttribute.get(this, 'http-equiv'); }
+  set httpEquiv(value) { stringAttribute.set(this, 'http-equiv', value); }
+
+  get content() { return stringAttribute.get(this, 'content'); }
+  set content(value) { stringAttribute.set(this, 'content', value); }
+
+  get charset() { return stringAttribute.get(this, 'charset'); }
+  set charset(value) { stringAttribute.set(this, 'charset', value); }
+
 }
 exports.HTMLMetaElement = HTMLMetaElement
+
+registerHTMLClass(tagName, HTMLMetaElement);
+
--- esm/html/meta-element.js
+++ esm/html/meta-element.js
@@ -1,10 +1,29 @@
 import {HTMLElement} from './element.js';
+import {registerHTMLClass} from '../shared/register-html-class.js';
+import { stringAttribute } from '../shared/attributes.js';
 
+const tagName = 'meta'
 /**
  * @implements globalThis.HTMLMetaElement
  */
 export class HTMLMetaElement extends HTMLElement {
-  constructor(ownerDocument, localName = 'meta') {
+  constructor(ownerDocument, localName =tagName) {
     super(ownerDocument, localName);
   }
+
+  get name() { return stringAttribute.get(this, 'name'); }
+  set name(value) { stringAttribute.set(this, 'name', value); }
+
+  get httpEquiv() { return stringAttribute.get(this, 'http-equiv'); }
+  set httpEquiv(value) { stringAttribute.set(this, 'http-equiv', value); }
+
+  get content() { return stringAttribute.get(this, 'content'); }
+  set content(value) { stringAttribute.set(this, 'content', value); }
+
+  get charset() { return stringAttribute.get(this, 'charset'); }
+  set charset(value) { stringAttribute.set(this, 'charset', value); }
+
 }
+
+registerHTMLClass(tagName, HTMLMetaElement);
+
