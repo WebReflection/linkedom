32a095fe1c2dc7ca29e57ed6ce197fb4a0e54afd
webreflection <andrea.giammarchi@gmail.com>
2023-03-14 11:32:19 +0100

    Updated all dependencies and devDependencies

--- types/commonjs/perf_hooks.d.cts
+++ types/commonjs/perf_hooks.d.cts
@@ -1,5 +1,1 @@
-/// <reference types="node" />
-export var performance: import("perf_hooks").Performance | {
-    now(): number;
-};
-import { performance } from "perf_hooks";
+export var performance: any;
--- types/esm/shared/node.d.ts
+++ types/esm/shared/node.d.ts
@@ -6,7 +6,7 @@ export function parentElement({ parentNode }: {
     parentNode: any;
 }): any;
 export function previousSibling({ [PREV]: prev }: {
-    "__@PREV@38498": any;
+    "__@PREV@22226": any;
 }): any;
 export function nextSibling(node: any): any;
 import { PREV } from "./symbols.js";
