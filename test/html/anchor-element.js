59e7165873a7e1845e8c95b2a7e129abf5769634
marcelreppi <marcelreppi@gmail.com>
2023-05-12 23:36:06 +0300

    Fix double encoding of already encoded character

--- test/html/anchor-element.js
+++ test/html/anchor-element.js
@@ -11,4 +11,5 @@ a.setAttribute('href', 'https://google.com/?q=1&page=2&test="');
 assert(a.toString(), '<a href="https://google.com/?q=1&page=2&test=&quot;">click me</a>');
 a.setAttribute('href', 'https://google.com/?q=asd&lol=<2>"');
 assert(a.href, 'https://google.com/?q=asd&lol=%3C2%3E%22');
-
+a.setAttribute('href', 'https://google.com/path%20to%20some%20file.pdf');
+assert(a.href, 'https://google.com/path%20to%20some%20file.pdf');
