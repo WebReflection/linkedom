bf82f103667edb1e193b0d71a7e7c20304c0e314
Wesley Luyten <me@wesleyluyten.com>
2023-06-04 16:48:13 -0500

    fix: register slot element, add skeleton

--- esm/html/slot-element.js
+++ esm/html/slot-element.js
@@ -1,10 +1,29 @@
 import {HTMLElement} from './element.js';
+import {registerHTMLClass} from '../shared/register-html-class.js';
+
+const tagName = 'slot';
 
 /**
  * @implements globalThis.HTMLSlotElement
  */
-export class HTMLSlotElement extends HTMLElement {
-  constructor(ownerDocument, localName = 'slot') {
+class HTMLSlotElement extends HTMLElement {
+  constructor(ownerDocument, localName = tagName) {
     super(ownerDocument, localName);
   }
+
+  /* c8 ignore start */
+  assign() {}
+
+  assignedNodes() {
+    return [];
+  }
+
+  assignedElements() {
+    return [];
+  }
+  /* c8 ignore stop */
 }
+
+registerHTMLClass(tagName, HTMLSlotElement);
+
+export {HTMLSlotElement};
