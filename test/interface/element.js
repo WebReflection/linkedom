989ad0ba8f7be6d5e61edafa59c47552b3d49e61
Omri Luzon <omril@testim.io>
2024-03-13 15:28:17 +0200

    coverage for xml empty attribute from set

--- test/interface/element.js
+++ test/interface/element.js
@@ -26,12 +26,12 @@ assert(htmlDoc.firstChild.getAttribute('content-desc'), 'text3&more');
 assert(htmlDoc.firstChild.outerHTML, '<span content-desc="text3&more"></span>');
 assert(htmlDoc.innerHTML, '<span content-desc="text3&more"></span>');
 
-htmlDoc.firstChild.setAttribute('content-desc', ''); // attribute not in emptyAttributes set is empty
+htmlDoc.firstChild.setAttribute('content-desc', ''); // attribute is not in emptyAttributes set is empty
 assert(htmlDoc.firstChild.getAttribute('content-desc'), '');
 assert(htmlDoc.firstChild.outerHTML, '<span content-desc=""></span>');
 assert(htmlDoc.innerHTML, '<span content-desc=""></span>');
 
-const htmlDocWithEmptyAttrFromSet = parser.parseFromString(`<div><span style=""/></div>`, 'text/html').documentElement; // attribute in emptyAttributes set is empty
+const htmlDocWithEmptyAttrFromSet = parser.parseFromString(`<div><span style=""/></div>`, 'text/html').documentElement; // attribute is in emptyAttributes set is empty
 
 assert(htmlDocWithEmptyAttrFromSet.firstChild.getAttribute('style'), '');
 assert(htmlDocWithEmptyAttrFromSet.firstChild.outerHTML, '<span></span>');
@@ -43,7 +43,12 @@ assert(xmlDoc.firstChild.getAttribute('content-desc'), 'text3&amp;more');
 assert(xmlDoc.firstChild.outerHTML, '<android.view.View content-desc="text3&amp;more" />');
 assert(xmlDoc.innerHTML, '<android.view.View content-desc="text3&amp;more" />');
 
-xmlDoc.firstChild.setAttribute('content-desc', '');
+xmlDoc.firstChild.setAttribute('content-desc', '');// attribute is not in emptyAttributes set is empty (even for XML)
 assert(xmlDoc.firstChild.getAttribute('content-desc'), '');
 assert(xmlDoc.firstChild.outerHTML, '<android.view.View content-desc="" />');
 assert(xmlDoc.innerHTML, '<android.view.View content-desc="" />');
+
+const xmlDocWithEmptyAttrFromSet = parser.parseFromString(`<hierarchy><android.view.View style=""/></hierarchy>`, 'text/xml').documentElement;// attribute is in emptyAttributes set is empty (even for XML)
+assert(xmlDocWithEmptyAttrFromSet.firstChild.getAttribute('style'), '');
+assert(xmlDocWithEmptyAttrFromSet.firstChild.outerHTML, '<android.view.View style="" />');
+assert(xmlDocWithEmptyAttrFromSet.innerHTML, '<android.view.View style="" />');
