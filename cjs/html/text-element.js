6612d19fa491cd296f4af0478b076de5063a1091
Whitewater <me@waterwater.moe>
2024-12-13 07:09:59 +0000

    fix: unexpected special replacement patterns

--- cjs/html/text-element.js
+++ cjs/html/text-element.js
@@ -10,7 +10,11 @@ class TextElement extends HTMLElement {
 
   toString() {
     const outerHTML = toString.call(this.cloneNode());
-    return outerHTML.replace(/></, `>${this.textContent}<`);
+    // Use `join` in here to prevent unexpected special replacement patterns
+    // See also 
+    // https://github.com/WebReflection/linkedom/issues/292
+    // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/replace#specifying_a_string_as_the_replacement
+    return outerHTML.split('><').join(`>${this.textContent}<`);
   }
 }
 exports.TextElement = TextElement
